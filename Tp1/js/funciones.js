3b15bad660afff8548f35d4d875eb30c0b1d684e
nachopanelo <nachopanelo27@gmail.com>
2023-04-15 16:41:53 -0300

    tp1

--- Tp1/js/funciones.js
+++ Tp1/js/funciones.js
@@ -1,4 +1,6 @@
 function boton(){
-    const aux = document.getElementById('cajatexto')
-    alert(aux.value);
+    const aux = document.getElementById('ap')
+    const aux1 = document.getElementById('nom')
+    const aux2 = document.getElementById('ed')
+    alert(aux.value + " " + aux1.value+ " " + aux2.value);
 }
