1cc5c794fcdc585ce9e94259ba1e3a1d465ac426
nachopanelo <129554199+nachopanelo@users.noreply.github.com>
2023-04-28 18:56:04 -0300

    Tp2

---
+++ Tp2/Js.js
@@ -0,0 +1,12 @@
+function getJugador(){
+    let jugador= {
+        nombre: "nahuel",
+        apellido: "bertulo",
+        club: "cerro porteño",
+        apodo: "cornu",
+        edad: "20",
+        activo: true,
+        historial: [],
+    }
+    console.log (jugador.club)
+}
