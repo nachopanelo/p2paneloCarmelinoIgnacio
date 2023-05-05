b51e03152ff6784a14746a448c59adeb5c4cbbad
nachopanelo <nachopanelo27@gmail.com>
2023-05-05 15:31:12 -0300

    dsad

--- Tp2/Js.js
+++ Tp2/Js.js
@@ -1,11 +1,16 @@
-
-
-function getCharacter() {
-fetch (`https://rickandmortyapi.com/api/character/1`)
-.then (function(response){
-  return response.json()
-})
-.then (function(data){
- console.log(data);
-})
+function getCharacter(){
+  let id = document.getElementById("CharBox").value
+  fetch("https://rickandmortyapi.com/api/character/"+id)
+      .then(function (response) {
+          return response.json()
+      })
+      .then(function (data) {
+          
+          document.getElementById("name").innerText = data.name;
+          document.getElementById("status").innerText = data.status;
+          document.getElementById("specie").innerText = data.species;
+          document.getElementById("city").innerText = data.location.name;
+          document.getElementById("foto").scrset = data.image;
+          console.log(data);
+      })
 }
