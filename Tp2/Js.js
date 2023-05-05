b6f2c053d8b0aeff7af01e30999a45ec57f82dee
nachopanelo <nachopanelo27@gmail.com>
2023-05-05 15:26:27 -0300

    tp

--- Tp2/Js.js
+++ Tp2/Js.js
@@ -1,15 +1,4 @@
-function getJugador(){
-    let jugador= {
-        nombre: "nahuel",
-        apellido: "bertulo",
-        club: "cerro porteño",
-        apodo: "cornu",
-        edad: "20",
-        activo: true,
-        historial: [],
-    }
-    console.log (jugador.club)
-}
+
 
 function getCharacter() {
 fetch (`https://rickandmortyapi.com/api/character/1`)
