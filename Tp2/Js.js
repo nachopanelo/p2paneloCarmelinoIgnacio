a83774e96aaec557f56682ef5f8ba7337b574b6f
nachopanelo <nachopanelo27@gmail.com>
2023-05-12 19:04:20 -0300

    tp 3 list

--- Tp2/Js.js
+++ Tp2/Js.js
@@ -1,29 +1,43 @@
-let infoa;
-fetch("https://rickandmortyapi.com/api/character/")
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        infoa = data.info.count;
-    })
-
+let pagAct = 0;
+let info;
 function getCharacter() {
-    let id = document.getElementById("CharBox").value
-    id = parseInt(id)
-    console.log(parseInt(id))
-    if (id >= 1 && id <= infoa.info.count) {
-        data = infoa.results[id-1]
-        document.getElementById("name").innerText = data.name;
-        document.getElementById("status").innerText = data.status;
-        document.getElementById("specie").innerText = data.species;
-        document.getElementById("city").innerText = data.location.name;
-        document.getElementById("foto").innerHTML = ` <img src="` + data.image + `">`;
-
+    let id = document.getElementById("CharBox").value;
+    id = parseInt(id);
+    pag = parseInt(id / 20) + 1;
+    let rId = (id % 20) - 1;
+    if (pag == pagAct) {
+        console.log('No Fetch');
+        let dat = info.results[rId]
+        document.getElementById("name").innerText = dat.name;
+        document.getElementById("status").innerText = dat.status;
+        document.getElementById("specie").innerText = dat.species;
+        document.getElementById("city").innerText = dat.location.name;
+        document.getElementById("foto").innerHTML = ` <img src="` + dat.image + `">`;
     } else {
-        document.getElementById("name").innerText = "Error: Personaje invalido";
-        document.getElementById("status").innerText = "";
-        document.getElementById("specie").innerText = "";
-        document.getElementById("city").innerText = "";
-        document.getElementById("foto").innerHTML = "";
+        console.log('Fecth');
+        fetch("https://rickandmortyapi.com/api/character/?page=" + pag)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                let dat = data.results[rId]
+                info = data;
+                console.log(dat);
+                document.getElementById("name").innerText = dat.name;
+                document.getElementById("status").innerText = dat.status;
+                document.getElementById("specie").innerText = dat.species;
+                document.getElementById("city").innerText = dat.location.name;
+                document.getElementById("foto").innerHTML = ` <img src="` + dat.image + `">`;
+                pagAct = pag;
+            })
+            .catch(function (error) {
+                document.getElementById("name").innerText = "Error: Personaje invalido";
+                document.getElementById("status").innerText = "";
+                document.getElementById("specie").innerText = "";
+                document.getElementById("city").innerText = "";
+                document.getElementById("foto").innerHTML = "";
+                console.log(error)
+            })
     }
+
 }
